e536b34d6a9d77e9dfd5d3d7b453093bb4f80e64
Ralph Wiest <lioncrazed@gmail.com>
2019-06-30 11:28:52 -0400

    added mongodb and add product

--- app.js
+++ app.js
@@ -4,18 +4,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const sequelize = require('./utils/database');
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
+const mongoConnect = require('./utils/database').mongoConnect;
 
 const app = express();
 
 const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const shopRoutes = require('./routes/shop');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -25,53 +19,22 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findByPk(1)
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => {
-      console.log(err);
-    });
+  // User.findByPk(1)
+  //   .then(user => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //   });
+  next();
 });
 
 app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.getNotFound);
 
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
-
-sequelize
-  // use .sync force to reinitialize the tables and
-  // .sync({ force: true })
-  .sync()
-  .then((result) => {
-    // create dummy user
-    return User.findByPk(1)
-  })
-  .then(user => {
-    if (!user) {
-      return User.create({ name: 'Ralph', email: 'rewiest@us.ibm.com' });
-    }
-    return user;
-    // note that adding 'Promise.resolve(user)' is optional here because
-    // when you return a value in a .then() block it is automatically wrapped into a Promise
-  })
-  .then(user => {
-    return user.createCart();
-  })
-  .then(cart => {
-    app.listen(3000);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+mongoConnect(() => {
+  app.listen(3000);
+});
--- utils/database.js
+++ utils/database.js
@@ -1,13 +1,30 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize(
-  'node-complete', 
-  'root', 
-  'wrpnst1492', 
-  {
-    dialect: 'mysql', 
-    host: 'localhost'
+let db;
+
+// method for connecting and storing (keeping alive) connection
+// MongoDB will automatically handle the pooling of connections for simultaneous needs
+const mongoConnect = callback => {
+  MongoClient.connect('mongodb+srv://lioncrazed:wrpnst1!@cluster0-ef34a.mongodb.net/shop?retryWrites=true&w=majority')
+  .then(client => {
+    console.log('Database Connected!');
+    db = client.db();
+    callback();
+  })
+  .catch(err => {
+    console.log(err);
+    throw err;
+  });
+}
+
+// method for getting access to the existing connection (if it exists)
+const getDb = () => {
+  if (db) {
+    return db;
   }
-);
+  throw 'No database found!';
+}
 
-module.exports = sequelize;
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
